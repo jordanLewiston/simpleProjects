8f9419ddbac82e5cf1195ac84d41f95f8cf6029a
Firecourt Corp <130227516+firecourtcorp@users.noreply.github.com>
2025-10-22 05:11:18 +0500

    initial commit

--- velocity&accelerationCalculator/main.swift
+++ velocity&accelerationCalculator/main.swift
@@ -1,11 +1,48 @@
-//
-//  main.swift
-//  velocity&accelerationCalculator
-//
-//  Created by Jordan Ponomarenko on 10/22/25.
-//
-
 import Foundation
 
-print("Hello, World!")
+struct Unit {
+    var distance: Double
+    var time: Double
+    var velocity: Double
+    var acceleration: Double
+}
+
+enum CalculationError: Error {
+    case invalidOperation
+}
+
+
+extension Unit {
+    mutating func calculate() throws -> Double {
+        if time != 0 && distance != 0 {
+            velocity = distance / time
+            return velocity
+        } else if time != 0 && velocity != 0 {
+            acceleration = velocity / time
+            return acceleration
+        } else {
+            throw CalculationError.invalidOperation
+        }
+    }
+}
+print ("Enter distance: ")
+let distanceInput = readLine()
+let distance = Double(distanceInput ?? "") ?? 0
+
+print ("Enter time: ")
+let timeInput = readLine()
+let time = Double(timeInput ?? "") ?? 0
+
+
+var unit = Unit(distance: distance, time: time, velocity: 0, acceleration: 0)
+
+do {
+    let result = try unit.calculate()
+    print("Result: \(result)")
+} catch {
+    print("Error: \(error)")
+}
+
+
+
 
